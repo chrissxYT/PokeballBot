82e7473531b97202dd2ffefea9f039af9994e34e
chrissx <35269695+chrissxYT@users.noreply.github.com>
2018-10-04 20:10:28 +0200

    Initial commit

---
+++ PokeballBot/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Forms;
+
+namespace PokeballBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 207);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private Button button1;
+        private PictureBox pictureBox1;
+    }
+}
+
---
+++ PokeballBot/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace PokeballBot
+{
+    public partial class Form1 : Form
+    {
+        bool running = false;
+
+        public Form1() => InitializeComponent();
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            running = !running;
+            button1.Text = running ? "Stop" : "Start";
+        }
+
+        void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (running)
+            {
+                Bitmap screenshot = ScreenshotPokeball;
+                Point ball = GetLoc(Color.FromArgb(201, 60, 76), screenshot);
+                Point pika = GetLoc(Color.FromArgb(248, 208, 80), screenshot);
+                pictureBox1.Image = screenshot;
+                if (ball.X != -1 && ball.Y != -1 && pika.X != -1 && pika.Y != -1)
+                    if (pika.X < ball.X)
+                        KeyboardPress(68, 7);
+                    else if (pika.Y > ball.Y)
+                        KeyboardPress(87, 26);
+                    else if (pika.X > ball.X)
+                        KeyboardPress(65, 4);
+                    else if (pika.Y < ball.Y)
+                        KeyboardPress(83, 22);
+            }
+            Invalidate(false);
+            Update();
+        }
+
+        Point GetLoc(Color c, Bitmap croppedGameScreen)
+        {
+            for (int x = 0; x < croppedGameScreen.Width; x++)
+                for (int y = 0; y < croppedGameScreen.Height; y++)
+                    if (AreAboutEqual(c, croppedGameScreen.GetPixel(x, y)))
+                        return new Point(x / 16, y / 16);
+            return new Point(-1, -1);
+        }
+
+        bool AreAboutEqual(Color f, Color s) => f.R - 10 < s.R && f.R + 10 > s.R && f.G - 10 < s.G && f.G + 10 > s.G && f.B - 10 < s.B && f.B + 10 > s.B;
+
+        static Bitmap ScreenshotPokeball
+        {
+            get
+            {
+                Rect rect = new Rect();
+                GetWindowRect(Process.GetProcessesByName("Pokeball").ToArray()[0].MainWindowHandle, ref rect);
+                Bitmap bmp = new Bitmap(160, 160, PixelFormat.Format24bppRgb);
+                Graphics.FromImage(bmp).CopyFromScreen(rect.left + 19, rect.top + 42, 0, 0, new Size(160, 160), CopyPixelOperation.SourceCopy);
+                return bmp;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct Rect
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        static void KeyboardPress(byte vk, byte sc)
+        {
+            keybd_event(vk, sc, 0, 0);
+            keybd_event(vk, sc, 2, 0);
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+    }
+}
